1847466c5b20f6acb08cc7716e129df3678d6932
Collin <Cflannery@student.csdspartans.org>
2017-03-21 09:16:55 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -1,5 +1,6 @@
 name_of_mydocument = 'tuesdayafternoon.txt'
 file_input = open(name_of_mydocument, 'r')     
+
 lines_in_file = -2
 line = file_input.readline()
 print(line, end = '')
@@ -7,8 +8,10 @@ lines_in_file = lines_in_file + 1
 line = file_input.readline()
 print(line)
 lines_in_file = lines_in_file + 1
+
 line = file_input.readline()
 line = file_input.readline()
+
 stanza_counter = 0
 while line != '':                  
   if line == "\n":
@@ -31,5 +34,5 @@ line = file_input.readline()
 
 print ("Total number of stanzas in this poem are: " + str(stanza_counter) + ".")
 print ("Total number of lines in this file are: 33.")
-print ("The Moody Blues members are Patrick Moraz, John Lodge, Ray Thomas, Justin Hayward, and  Graeme Edge.")
-print("The song \"Tuesday Afternoon\" first appeared on the album \033[4mDays of Future Passed\033[0m in 1967.")
+print ("The Moody Blues members are Patrick Moraz, John Lodge, Ray Thomas, Justin Hayward, Graeme Edge, and Mike Pinder.")
+print("The song \"Tuesday Afternoon\" first appeared on the album \033[3mDays of Future Passed\033[0m in 1967.")
